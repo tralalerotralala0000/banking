555f7e62cc0f145e90d0f3718e59f33fdbb51597
Camilo Criollo <criolloc449@gmail.com>
2025-09-11 00:33:01 -0500

    finalizacion

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Country, Department, City
+
+# Registra modelos
+admin.site.register(Country)
+admin.site.register(Department)
+admin.site.register(City)
 
 # Register your models here.
